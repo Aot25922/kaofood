c981552f7fa37b517f073ff189ceb9b1f2d97830
nithach2321 <bom-_2321@outlook.co.th>
2021-08-19 00:43:56 +0700

    Update docker & init

--- database/init.sql
+++ database/init.sql
@@ -1,72 +1,59 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 GRANT ALL ON *.* to 'int222'@'%' WITH GRANT OPTION;
-SET GLOBAL max_connections = 1000;
 
-DROP SCHEMA IF EXISTS `9shoes` ;
-CREATE SCHEMA IF NOT EXISTS `9shoes` DEFAULT CHARACTER SET utf8 ;
-USE `9shoes` ;
+DROP SCHEMA IF EXISTS `9food` ;
+CREATE SCHEMA IF NOT EXISTS `9food` DEFAULT CHARACTER SET utf8 ;
+USE `9food` ;
 
-DROP TABLE orderDetail;
-DROP TABLE user;
-DROP TABLE userDetail;
-DROP TABLE menu;
-DROP TABLE type;
-
-CREATE TABLE orderDetail (
-    orderId    CHAR(10) NOT NULL AUTO_INCREMENT,
-    totalPrice    DECIMAL(6,2) NOT NULL,
-    menuId    CHAR(10) NOT NULL,
-    userId    CHAR(10) NOT NULL
-);
-ALTER TABLE orderDetail ADD CONSTRAINT order_pk PRIMARY KEY ( orderId );
-
-CREATE TABLE user (
-    userId    CHAR(10) NOT NULL,
-    userName    VARCHAR(45) NOT NULL,
-    password    VARCHAR(45) NOT NULL,
-    userDetailId    CHAR(10) NOT NULL
-);
-ALTER TABLE user ADD CONSTRAINT user_pk PRIMARY KEY ( userId );
+DROP TABLE IF EXISTS `9food`.`orderDetail` ;
+DROP TABLE IF EXISTS `9food`.`user` ;
+DROP TABLE IF EXISTS `9food`.`userDetail` ;
+DROP TABLE IF EXISTS `9food`.`menu` ;
+DROP TABLE IF EXISTS `9food`.`type` ;
 
 CREATE TABLE userDetail (
-    userDetailId    CHAR(10) NOT NULL,
-    fname    VARCHAR(45) NOT NULL,
-    lname    VARCHAR(45) NOT NULL,
-    dob	Date NOT NULL,
-    address	VARCHAR(200),
-    role	VARCHAR(15) NOT NULL
+    userDetailId    CHAR(10) 	NOT NULL,
+    fname    		VARCHAR(45) NOT NULL,
+    lname    		VARCHAR(45) NOT NULL,
+    dob				Date 		NOT NULL,
+    address			VARCHAR(200),
+    role			VARCHAR(15) NOT NULL,
+    CONSTRAINT user_detail_pk PRIMARY KEY ( userDetailId )
 );
-ALTER TABLE userDetail ADD CONSTRAINT user_detail_pk PRIMARY KEY ( userDetailId );
 
-CREATE TABLE menu (
-    menuId    CHAR(10) NOT NULL AUTO_INCREMENT,
-    menuName    VARCHAR(45) NOT NULL,
-    price    DECIMAL(5,2) NOT NULL,
-    description    VARCHAR(200) NOT NULL,
-    image	VARCHAR(50) NOT NULL,
-    typeId	CHAR(10) NOT NULL
+CREATE TABLE user (
+    userId    		VARCHAR(10) NOT NULL,
+    userName    	VARCHAR(45) NOT NULL,
+    password    	VARCHAR(45) NOT NULL,
+    userDetailId    CHAR(10) 	NOT NULL,
+	CONSTRAINT user_pk PRIMARY KEY ( userId ),
+    CONSTRAINT user_detail_fk FOREIGN KEY ( userDetailId ) REFERENCES userDetail ( userDetailId )
 );
-ALTER TABLE menu ADD CONSTRAINT menu_pk PRIMARY KEY ( menuId );
 
 CREATE TABLE type (
-    typeId    CHAR(10) NOT NULL,
-    typeName    VARCHAR(20) NOT NULL,
-    description    VARCHAR(100) NOT NULL
+    typeId    		CHAR(10) 	NOT NULL,
+    typeName    	VARCHAR(20) NOT NULL,
+    image			VARCHAR(50) NOT NULL,
+    description    	VARCHAR(100),
+    CONSTRAINT type_pk PRIMARY KEY ( typeId )
 );
-ALTER TABLE type ADD CONSTRAINT type_pk PRIMARY KEY ( typeId );
 
+CREATE TABLE menu (
+    menuId    		INT(10) 		NOT NULL AUTO_INCREMENT,
+    menuName    	VARCHAR(45)	 	NOT NULL,
+    price    		DECIMAL(5,2) 	NOT NULL,
+    description    	VARCHAR(200) 	NOT NULL,
+    image			VARCHAR(50) 	NOT NULL,
+    typeId			CHAR(10) 		NOT NULL,
+    CONSTRAINT menu_pk PRIMARY KEY ( menuId ),
+    CONSTRAINT type_fk FOREIGN KEY ( typeId ) REFERENCES type ( typeId )
+);
 
-ALTER TABLE user
-    ADD CONSTRAINT user_detail_fk FOREIGN KEY ( userDetailId )
-        REFERENCES userDetail ( userDetailId );
-ALTER TABLE orderDetail
-    ADD CONSTRAINT user_fk FOREIGN KEY ( userId )
-        REFERENCES user ( userId );
-ALTER TABLE orderDetail
-    ADD CONSTRAINT menu_fk FOREIGN KEY ( menuId )
-        REFERENCES menu ( menuId );
-ALTER TABLE menu
-    ADD CONSTRAINT type_fk FOREIGN KEY ( typeId )
-        REFERENCES type ( typeId );
+CREATE TABLE orderDetail (
+    orderId    INT(10) 	NOT NULL AUTO_INCREMENT,
+    totalPrice DECIMAL(6,2) NOT NULL,
+    menuId     INT(10) 	NOT NULL,
+    userId     VARCHAR(10) 	NOT NULL,
+    CONSTRAINT order_pk PRIMARY KEY ( orderId ),
+    CONSTRAINT user_fk FOREIGN KEY ( userId ) REFERENCES user ( userId ),
+    CONSTRAINT menu_fk FOREIGN KEY ( menuId ) REFERENCES menu ( menuId )
+);
