5451719f7ac9bd35175d3cd545b78be4cd31f32f
thanasapL <thanasap.lee@outlook.com>
2021-08-26 23:10:31 +0700

    add init, insert

--- database/init.sql
+++ database/init.sql
@@ -7,20 +7,20 @@ DROP TABLE IF EXISTS [type] ;
 
 
 CREATE TABLE userDetail (
-    userDetailId    CHAR(10) 	NOT NULL,
+    userDetailId    INT IDENTITY(1,1) 	NOT NULL,
     fname    		VARCHAR(45) NOT NULL,
     lname    		VARCHAR(45) NOT NULL,
-    phone           VARCHAR(10) NOT NULL,
+    phone           VARCHAR(15) NOT NULL,
     address			VARCHAR(200),
     role			VARCHAR(15) NOT NULL,
     CONSTRAINT user_detail_pk PRIMARY KEY ( userDetailId )
 );
 
 CREATE TABLE [user] (
-    userId    	    CHAR(10)    NOT NULL,
-    email    	    VARCHAR(45) NOT NULL,
-    password    	VARCHAR(45) NOT NULL,
-    userDetailId    CHAR(10) 	NOT NULL,
+    userId    	    INT IDENTITY(1,1)    NOT NULL,
+    email    	    VARCHAR(45)         NOT NULL,
+    password    	VARCHAR(45)         NOT NULL,
+    userDetailId    INT	                NOT NULL,
 	CONSTRAINT user_pk PRIMARY KEY ( userId ),
     CONSTRAINT user_email_uk UNIQUE( email ),
     CONSTRAINT user_detail_fk FOREIGN KEY ( userDetailId ) REFERENCES userDetail ( userDetailId )
@@ -38,7 +38,7 @@ CREATE TABLE [menu] (
     menuId    		INT IDENTITY(1,1) NOT NULL,
     menuName    	VARCHAR(45)	 	  NOT NULL,
     price    		DECIMAL(5,2) 	  NOT NULL,
-    description    	VARCHAR(200) 	  NOT NULL,
+    description    	VARCHAR(1000) 	  NOT NULL,
     image			VARCHAR(50) 	  NOT NULL,
     typeId			CHAR(10) 		  NOT NULL,
     CONSTRAINT menu_pk PRIMARY KEY ( menuId ),
@@ -49,7 +49,7 @@ CREATE TABLE [order] (
     orderId    INT IDENTITY(1,1)    NOT NULL,
     totalPrice DECIMAL(6,2)         NOT NULL,
     menuId     INT  	            NOT NULL,
-    userId     CHAR(10) 	        NOT NULL,
+    userId     INT 	        NOT NULL,
     CONSTRAINT order_pk PRIMARY KEY ( orderId ),
     CONSTRAINT user_fk  FOREIGN KEY ( userId ) REFERENCES [user] ( userId ),
     CONSTRAINT menu_fk  FOREIGN KEY ( menuId ) REFERENCES [menu] ( menuId )
--- database/insert.sql
+++ database/insert.sql
@@ -1,13 +1,16 @@
 -- UserDetail
-INSERT INTO USERDETAIL(userDetailId, fname, lname, email, phone, address, role) VALUES ('1001', 'President', 'CEO', 'kaofood@mail.com', '0-112-2021-2564');
+INSERT INTO [USERDETAIL](fname, lname, phone, address, role) VALUES ('President', 'CEO', '0-112-2021-2564', 'KaoHome', 'Admin');
+INSERT INTO [USERDETAIL](fname, lname, phone, address, role) VALUES ('Tutoom', 'Manyriver', '0922253239', 'Home', 'Customer');
 
 -- User 
-INSERT INTO USER(userId, userName, password, userDetailId) VALUES ('10001', 'admin', 'password', '1001');
-
--- Menu
-INSERT INTO MENU(munuId, menuName, price, description, image, typeId) VALUES ('1', 'Beef Noodle Soup', '40.50', 'Wonderfully fragrant and deliciously healthy, this simple Chinese Beef Noodle Soup makes the perfect warming meal.', '/images/menu/noodleSoup.jpg', '1');
-INSERT INTO MENU(munuId, menuName, price, description, image, typeId) VALUES ('2', 'Spicy Shrimp Soup', '120.00', 'Tom Yum is probably the most famous of Thai soups and is popular not only in Thailand but in Thai restaurants worldwide. It is a clear, sour soup flavored with fragrant lemon grass, fresh galangal root and kaffir lime leaf. This potent herbal mixture is well known for its medicinal properties.', '/images/menu/spicyShrimjpSoup.jpg', '2');
+INSERT INTO [USER](email, password, userDetailId) VALUES ('admin', 'password', 1);
+INSERT INTO [USER](email, password, userDetailId) VALUES ('tu@mail.com', 'k904112', 2);
 
 -- Type
-INSERT INTO TYPE(typeId, typeName, image, description) VALUES ('1', 'Noodle', '/images/type/noode.png');
-INSERT INTO TYPE(typeId, typeName, image, description) VALUES ('1', 'Soup', '/images/type/Soup.png');
+INSERT INTO [TYPE](typeId, typeName, image, description) VALUES ('1', 'Noodle', '/images/type/noode.png', null);
+INSERT INTO [TYPE](typeId, typeName, image, description) VALUES ('2', 'Soup', '/images/type/Soup.png', null);
+
+-- Menu
+INSERT INTO [MENU](menuName, price, description, image, typeId) VALUES ('Beef Noodle Soup', 40.50, 'Wonderfully fragrant and deliciously healthy, this simple Chinese Beef Noodle Soup makes the perfect warming meal.', '/images/menu/noodleSoup.jpg', '1');
+INSERT INTO [MENU](menuName, price, description, image, typeId) VALUES ('Spicy Shrimp Soup', 120.00, 'Tom Yum is probably the most famous of Thai soups and is popular not only in Thailand but in Thai restaurants worldwide. It is a clear, sour soup flavored with fragrant lemon grass, fresh galangal root and kaffir lime leaf. This potent herbal mixture is well known for its medicinal properties.', '/images/menu/spicyShrimjpSoup.jpg', '2');
+
