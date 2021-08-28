1a428d3ed9de17ce9bb51b099625e7ea1f4818f0
nithach2321 <nithach@outlook.co.th>
2021-08-28 19:25:02 +0700

    Move MsSql to MySQL

--- database/init.sql
+++ database/init.sql
@@ -1,11 +1,14 @@
+GRANT ALL ON *.* to 'int222'@'%' WITH GRANT OPTION;
+DROP SCHEMA IF EXISTS `kaofood` ;
+CREATE SCHEMA IF NOT EXISTS `kaofood` DEFAULT CHARACTER SET utf8 ;
 USE kaofood;
-DROP TABLE IF EXISTS [order];
-DROP TABLE IF EXISTS [user] ;
-DROP TABLE IF EXISTS [menu] ;
-DROP TABLE IF EXISTS [category] ;
+DROP TABLE IF EXISTS `order`;
+DROP TABLE IF EXISTS `user` ;
+DROP TABLE IF EXISTS `menu` ;
+DROP TABLE IF EXISTS `category` ;
 
-CREATE TABLE [user] (
-    userId    	    INT IDENTITY(1,1)    NOT NULL,
+CREATE TABLE `user` (
+    userId    	    INT                 NOT NULL AUTO_INCREMENT,
     email    	    VARCHAR(45)         NOT NULL,
     password    	VARCHAR(45)         NOT NULL,
     fname    		VARCHAR(45)         NOT NULL,
@@ -18,16 +21,16 @@ CREATE TABLE [user] (
     CONSTRAINT user_phone UNIQUE ( phone )
 );
 
-CREATE TABLE [category] (
-    cateId    		INT IDENTITY(1,1) 	NOT NULL,
+CREATE TABLE `category` (
+    cateId    		INT 				NOT NULL AUTO_INCREMENT,
     cateName    	VARCHAR(20)         NOT NULL,
     image			VARCHAR(50)         NOT NULL,
     description    	VARCHAR(100),
     CONSTRAINT cate_pk PRIMARY KEY ( cateId )
 );
 
-CREATE TABLE [menu] (
-    menuId    		INT IDENTITY(1,1) NOT NULL,
+CREATE TABLE `menu` (
+    menuId    		INT				  NOT NULL AUTO_INCREMENT,
     menuName    	VARCHAR(45)	 	  NOT NULL,
     price    		DECIMAL(5,2) 	  NOT NULL,
     description    	VARCHAR(1000) 	  NOT NULL,
@@ -37,12 +40,12 @@ CREATE TABLE [menu] (
     CONSTRAINT cate_fk FOREIGN KEY ( cateId ) REFERENCES category ( cateId )
 );
 
-CREATE TABLE [order] (
-    orderId    INT IDENTITY(1,1)    NOT NULL,
+CREATE TABLE `order` (
+    orderId    INT					NOT NULL AUTO_INCREMENT,
     totalPrice DECIMAL(6,2)         NOT NULL,
     menuId     INT  	            NOT NULL,
     userId     INT 	        NOT NULL,
     CONSTRAINT order_pk PRIMARY KEY ( orderId ),
-    CONSTRAINT user_fk  FOREIGN KEY ( userId ) REFERENCES [user] ( userId ),
-    CONSTRAINT menu_fk  FOREIGN KEY ( menuId ) REFERENCES [menu] ( menuId )
+    CONSTRAINT user_fk  FOREIGN KEY ( userId ) REFERENCES `user` ( userId ),
+    CONSTRAINT menu_fk  FOREIGN KEY ( menuId ) REFERENCES `menu` ( menuId )
 );
